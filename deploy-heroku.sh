25fd88000cdaf989542341cfba6d2bbb944d7a77
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2016-12-03 16:25:25 +0100

    sasas

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Appname: ${heroku-app-name-base}"
 gradle shadowJar
 heroku apps:create larkj-app1-${CIRCLE_BRANCH} --no-remote --region eu
 heroku deploy:jar build/libs/skeleton-prototype-fat.jar --app larskj-app1-${CIRCLE_BRANCH}
